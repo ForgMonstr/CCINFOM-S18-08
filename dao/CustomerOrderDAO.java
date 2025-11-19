01b49837077a3d088e8b76d076a72a8922be4c4e
Leo <nopeso290@gmail.com>
2025-11-19 20:54:17 +0800

    Added CustomerOrderDAO.java

--- dao/CustomerOrderDAO.java
+++ dao/CustomerOrderDAO.java
@@ -1,10 +1,17 @@
 package dao;
 
+import DBConnection.DBConnector;
+import models.order.CustomerOrder;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class CustomerOrderDAO {
 
     public void insertOrder(CustomerOrder o) {
         String sql = "INSERT INTO customerorder (order_date, branch_id, total_amount) VALUES (?, ?, ?)";
-        try (Connection conn = DatabaseConnection.getConnection();
+        try (Connection conn = DBConnector.getConnection();
              PreparedStatement stmt = conn.prepareStatement(sql)) {
 
             stmt.setDate(1, o.getOrderDate());
@@ -17,7 +24,7 @@ public class CustomerOrderDAO {
 
     public CustomerOrder getOrderById(int id) {
         String sql = "SELECT * FROM customerorder WHERE order_id = ?";
-        try (Connection conn = DatabaseConnection.getConnection();
+        try (Connection conn = DBConnector.getConnection();
              PreparedStatement stmt = conn.prepareStatement(sql)) {
 
             stmt.setInt(1, id);
@@ -40,7 +47,7 @@ public class CustomerOrderDAO {
         List<CustomerOrder> list = new ArrayList<>();
         String sql = "SELECT * FROM customerorder";
 
-        try (Connection conn = DatabaseConnection.getConnection();
+        try (Connection conn = DBConnector.getConnection();
              Statement stmt = conn.createStatement();
              ResultSet rs = stmt.executeQuery(sql)) {
 
@@ -59,7 +66,7 @@ public class CustomerOrderDAO {
 
     public void updateOrder(CustomerOrder o) {
         String sql = "UPDATE customerorder SET order_date=?, branch_id=?, total_amount=? WHERE order_id=?";
-        try (Connection conn = DatabaseConnection.getConnection();
+        try (Connection conn = DBConnector.getConnection();
              PreparedStatement stmt = conn.prepareStatement(sql)) {
 
             stmt.setDate(1, o.getOrderDate());
@@ -73,7 +80,7 @@ public class CustomerOrderDAO {
 
     public void deleteOrder(int id) {
         String sql = "DELETE FROM customerorder WHERE order_id=?";
-        try (Connection conn = DatabaseConnection.getConnection();
+        try (Connection conn = DBConnector.getConnection();
              PreparedStatement stmt = conn.prepareStatement(sql)) {
 
             stmt.setInt(1, id);
